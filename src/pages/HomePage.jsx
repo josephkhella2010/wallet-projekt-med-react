89077fb4c8637fa1d7e870d98030920de4b57b23
joseph khella <josephkhella2010@gmail.com>
2024-10-14 08:50:36 +0200

    uptodate

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,21 @@ import React from "react";
 import CardComponent from "../components/CardComponent";
 import { Link } from "react-router-dom";
 import SettingIcon from "../components/SettingIcon";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+  const data = useSelector((store) => store.cardData);
+  const navigate = useNavigate();
+
+  function handleAdd() {
+    if (data.cardDetails.length < 4) {
+      navigate("/addCard");
+    } else {
+      alert("u cannot add more than 4 card");
+      return;
+    }
+  }
   return (
     <div className="Home-Page">
       <div className="wrapper">
@@ -11,9 +24,10 @@ export default function HomePage() {
         <h1>Cards that u have</h1>
         <CardComponent />
         <div className="button">
-          <Link to="/addCard" className="link">
-            <button>Add New Card</button>
-          </Link>
+          {/*  <Link to="/addCard" className="link">
+            <button onClick={handleAdd}>Add New Card</button>
+          </Link> */}
+          <button onClick={handleAdd}>Add New Card</button>
         </div>
       </div>
     </div>
